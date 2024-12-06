1e6a168b5b2a70db92dbc546e1b58a5b99df8a5f
janisaiad <janisaiad.ja@gmail.com>
2024-12-06 13:07:18 +0000

    added mergesh

---
+++ merge.sh
@@ -0,0 +1,21 @@
+# Pour dev-janis
+git checkout dev-janis
+git merge dev
+
+# Pour dev-flo
+git checkout dev-flo
+git merge dev
+
+# Pour dev-bap
+git checkout dev-bap
+git merge dev
+
+# Pour dev-benoit
+git checkout dev-benoit
+git merge dev
+
+git push origin dev-janis
+git push origin dev-flo
+git push origin dev-bap
+git push origin dev-benoit
+
